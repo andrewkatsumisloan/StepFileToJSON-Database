db001dc572794387d2513de5c738a5bfad3780cf
Andrew Katsumi Sloan <30994664+andrewksloan@users.noreply.github.com>
2020-08-24 07:31:29 -0700

    Update parse_step_final.py

--- parse_step_final.py
+++ parse_step_final.py
@@ -5,8 +5,8 @@ To use program, run via command line: python3 parse_args.py <name of step file>.
 Where <key> is either a string or integer value by which you want to query, or a quoted label number preceded by a pound key (i.e '#18')
 
 There are two different types of arguments this program accepts
-    1. a "label number" (i.e '#39'), which will return a nested list of the line and the references that each line contains
-    2. a string (not case sensitive), int, or float, which will return every line in which that string appears.
+    1. a "label number" (i.e '#39'), which will return a nested list of the line and the references (expanded) that each line contains
+    2. a string (not case sensitive), int, or float, which will return every line in which it appears.
 
 Examples:
 LINUX TERMINAL
